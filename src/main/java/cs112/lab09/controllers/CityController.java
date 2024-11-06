9f1e44e9b280cde46fb454345b30424fa2cbb19a
MCCLab4804WD23 <ianmclaughlin115@gmail.com>
2024-11-06 14:52:59 -0800

    Brother Hmm?

--- src/main/java/cs112/lab09/controllers/CityController.java
+++ src/main/java/cs112/lab09/controllers/CityController.java
@@ -1,4 +1,26 @@
 package cs112.lab09.controllers;
 
+import cs112.lab09.Constants;
+import cs112.lab09.models.RevisedHistoricalEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.WindowEvent;
+
+import static cs112.lab09.Constants.HISTORICAL_DATA;
+import static cs112.lab09.Constants.REDSUMMER_IMAGE_PATH;
+
 public class CityController {
+    @FXML
+    Label locationLabel;
+    public void initData(Constants.Event eventIndex){
+        String[] data = HISTORICAL_DATA[eventIndex.ordinal()];
+        RevisedHistoricalEvent event = new RevisedHistoricalEvent(data);
+        event.getImage();
+    }
+
+    public void handleCloseWindow(WindowEvent e){
+        dispose();
+    }
 }
